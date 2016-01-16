9fc644248c7e5cf36ecb82d37685866ca5008ffb
andela-mfalade <mayowa.falade@andela.co>
2016-01-17 00:09:03 +0100

    Remove api keys from script

--- udacity_tuts/send_sms.py
+++ udacity_tuts/send_sms.py
@@ -1,13 +1,15 @@
+import os
 from twilio.rest import TwilioRestClient
 
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACeeb7428aca8d1a9e8b4a26c5b9890a4c"
-auth_token = "5f5d3f71ab3d25d7d5ebc7f53d2adb47"
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+twilio_phone_num = os.environ['TWILIO_PHONE_NUMBER']
 client = TwilioRestClient(account_sid, auth_token)
 message = client.messages.create(
     body="Hey Mayor, Go and eat! ;)",
     to="+2348137219945",
-    from_="+12516629160"
+    from_=""
 )
 print message.sid
